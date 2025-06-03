c288efd2fa685360fffd538528c1d9af432c2557
Imandi <imandikavithma2714@gmail.com>
2025-06-03 21:53:10 +0530

    Added histogram display using graphics.py

--- predictor.py
+++ predictor.py
@@ -31,3 +31,92 @@ def calculate_outcome(pass_credits, defer_credits, fail_credits):
         return("Do not progress - module retriever")   
     else:
         return("Exclude")
+
+def display_histogram(progress_count,trailer_count, retriever_count, exclude_count):
+    win = GraphWin("Histogram",700,600)
+
+    b1len = 400-(progress_count*25)
+    b2len = 400-(trailer_count*25)
+    b3len = 400-(retriever_count*25)
+    b4len = 400-(exclude_count*25)
+
+    Title1 = Text(Point(150,50),"Histogram Results")
+    Title1.setStyle("bold")
+    Title1.setSize(16)
+    Title1.draw(win)
+
+    Title2 = Text(Point(150,500),f"Total Student = {progress_count + trailer_count + retriever_count + exclude_count }")
+    Title2.setStyle("bold")
+    Title2.setSize(16)
+    Title2.draw(win)
+
+    #line
+    Xline = Line(Point(50,400), Point(650,400))
+    Xline.draw(win)
+
+    #Text1
+    lebel = Text(Point(90,420),"Progress")
+    lebel.setStyle("bold")
+    lebel.setSize(12)
+    lebel.draw(win)
+
+    #Upcount1
+    b1name = Text(Point(90,b1len-10),progress_count)
+    b1name.draw(win)
+
+    #Rectangle1
+    box1 = Rectangle(Point(60,400), Point(120,b1len))
+    box1.draw(win)
+    box1.setFill("green")
+
+
+    #Text2
+    lebel2 = Text(Point(240,420),"Trailer")
+    lebel2.setStyle("bold")
+    lebel2.setSize(12)
+    lebel2.draw(win)
+
+    #Upcount2
+    b2name = Text(Point(240,b2len-10),trailer_count)
+    b2name.draw(win)
+
+    #Rectangle2
+    box2 = Rectangle(Point(210,400), Point(270,b2len))
+    box2.draw(win)
+    box2.setFill("yellow")
+
+    #Text3
+    lebel3 = Text(Point(390,420),"Retriever")
+    lebel3.setStyle("bold")
+    lebel3.setSize(12)
+    lebel3.draw(win)
+
+    #Upcount3
+    b3name = Text(Point(390,b3len-10),retriever_count)
+    b3name.draw(win)
+
+    #Rectangle3
+    box3 = Rectangle(Point(360,400), Point(420,b3len))
+    box3.draw(win)
+    box3.setFill("blue")
+
+    #Text4
+    lebel4 = Text(Point(540,420),"Exclude")
+    lebel4.setStyle("bold")
+    lebel4.setSize(12)
+    lebel4.draw(win)
+
+    #Upcount4
+    b4name = Text(Point(540,b4len-10),exclude_count)
+    b4name.draw(win)
+
+    #Rectangle4
+    box4 = Rectangle(Point(510,400), Point(570,b4len))
+    box4.draw(win)
+    box4.setFill("red")
+
+    try:
+        win.getMouse()
+        win.close()
+    except:
+        pass

caa1d27f38f1994ae4b6320b78d33c88c63c00a2
Imandi <imandikavithma2714@gmail.com>
2025-06-03 17:57:06 +0530

    Implemented progression outcome calculation logic

--- predictor.py
+++ predictor.py
@@ -9,3 +9,25 @@ def validate_input(credits):
     except ValueError:
         print("Integer required.")
         return False
+
+#calculate the outcome based on credit values
+def calculate_outcome(pass_credits, defer_credits, fail_credits):
+    total_credits = pass_credits + defer_credits + fail_credits
+    if total_credits != 120:
+        print("Total incorrect.")
+        return "Invalid"
+    
+    if pass_credits == 120:
+        return("progress")
+    elif pass_credits == 100:
+        return("progress (module trailer)") 
+    elif pass_credits >= 60:
+        return("Do not progress - module retriever")
+    elif pass_credits == 40 and defer_credits >= 20 and fail_credits <= 60:
+        return("Do not progress - module retriever")
+    elif pass_credits == 20 and defer_credits >= 40 and fail_credits <= 60:
+        return("Do not progress - module retriever")  
+    elif pass_credits == 0 and defer_credits >= 60 and fail_credits <= 60:
+        return("Do not progress - module retriever")   
+    else:
+        return("Exclude")
